4aea57e62176c2c335dbfb9e889776208f22006e
AleemKhokhar <aleem.khokhar10@gmail.com>
2026-02-01 11:20:53 +0400

    Search bar sends me somewhere

--- core/views.py
+++ core/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+
 def home(request):
     return render(request, "core/home.html")
+
+def game_search(request):
+    query = request.GET.get('q')
+    print(f"DEBUG: User searched for {query}")
+    
+    if query:
+        return HttpResponse(f"The Brain caught your word! You are looking for: {query}")
+    else:
+        return HttpResponse("Please type something in the search box!")
