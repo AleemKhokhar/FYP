ab09c09ed02bf0180a3b7b06c8875bfcc59ab037
AleemKhokhar <160232972+AleemKhokhar@users.noreply.github.com>
2026-04-23 09:33:37 +0100

    Update settings.py

--- web_project/settings.py
+++ web_project/settings.py
@@ -37,7 +37,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',   
     'django.contrib.sessions.middleware.SessionMiddleware',
-    ... (rest unchanged)
     ]
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
