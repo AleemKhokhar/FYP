70ef6b7d44f29432792582cb32b4d1249b0a6ca7
AleemKhokhar <aleem.khokhar10@gmail.com>
2026-05-31 18:23:52 +0100

    database

--- web_project/settings.py
+++ web_project/settings.py
@@ -70,7 +70,7 @@ ASGI_APPLICATION = 'web_project.asgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
         conn_max_age=600,
     )
 }
