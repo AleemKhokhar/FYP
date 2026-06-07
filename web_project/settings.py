ed70619e0121d03863a1e30b7adf9c1f9e151aa3
AleemKhokhar <aleem.khokhar10@gmail.com>
2026-06-07 08:04:28 +0100

    potential fix v2?

--- web_project/settings.py
+++ web_project/settings.py
@@ -21,7 +21,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'livereload',
     'cloudinary_storage',
     'cloudinary',
     'django.contrib.sites',
