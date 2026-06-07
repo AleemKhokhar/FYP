02e4954778256d56628dee7e9518220bd43a67c2
AleemKhokhar <aleem.khokhar10@gmail.com>
2026-06-07 07:25:24 +0100

    Accessibility/orientation

--- web_project/settings.py
+++ web_project/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'livereload',
     'cloudinary_storage',
     'cloudinary',
     'django.contrib.sites',
